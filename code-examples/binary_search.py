335ee561ffb3a78b4bdee216b558fadba39c3fb1
Joe Strauss <cuppajava@tutanota.com>
2021-11-23 10:07:13 -0600

    Added binary_search.py example

---
+++ code-examples/binary_search.py
@@ -0,0 +1,23 @@
+import random
+
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, -1))
+
+print(binary_search(range(1, 100000000), random.randint(1, 100000000)))
