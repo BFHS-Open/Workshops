d7bf0487065fdfabb87c2dc7703c99fd6ddade1d
Thomas W <87614336+YesAintNo@users.noreply.github.com>
2021-12-08 16:40:19 -0600

    Using Joe's (Better) Code
    Also added some 'tests'

--- CS_Ed_Week/code-examples/filter_under_number.py
+++ CS_Ed_Week/code-examples/filter_under_number.py
@@ -1,8 +1,11 @@
 def Filter_Under_Number(input_number, input_list):
   temp_list = []
-  for x in range(len(input_list)):
-    if input_list[x] < input_number:
-      temp_list.append(input_list[x])
+  for x in input_list:
+    if x < input_number:
+    temp_list.append(x)
   return temp_list
 
-print(Filter_Under_Number(5, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]))
+"""Tests"""
+print(Filter_Under_Number(5, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # [1,2,3,4]
+print(Filter_Under_Number(-2, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # []
+print(Filter_Under_Number(12, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # [1,2,3,4,5,6,7,8,9]
