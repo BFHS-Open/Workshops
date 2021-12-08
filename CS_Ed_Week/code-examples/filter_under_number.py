f5e582e17c87243b84aa6d2f6805d26f8c7d17d2
Thomas W <87614336+YesAintNo@users.noreply.github.com>
2021-12-08 16:51:33 -0600

    Remember how numbers work

--- CS_Ed_Week/code-examples/filter_under_number.py
+++ CS_Ed_Week/code-examples/filter_under_number.py
@@ -8,4 +8,4 @@ def Filter_Under_Number(input_number, input_list):
 """Tests"""
 print(Filter_Under_Number(5, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # [1,2,3,4]
 print(Filter_Under_Number(-2, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # []
-print(Filter_Under_Number(12, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # [1,2,3,4,5,6,7,8,9]
+print(Filter_Under_Number(12, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])) # [1,2,3,4,5,6,7,8,9,10,11]
